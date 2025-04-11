f1c09184f94d78276bd5bb75f6d374c7578d8ecb
Ryan Lee <49077558+ryanlee0215@users.noreply.github.com>
2025-04-11 17:46:13 -0400

    Update RegisterExecutor.hpp - Added LRU
    Added LRU eviction strategy

--- Execution/RegisterExecutor.hpp
+++ Execution/RegisterExecutor.hpp
@@ -33,10 +33,15 @@ public:
         }
     }
 
-    bool isValid([[maybe_unused]] uint8_t addr, [[maybe_unused]] mlir::Value value) const
+    bool isValid([[maybe_unused]] uint8_t addr, [[maybe_unused]] mlir::Value value)
     {
 #ifdef TMP_REGISTER_COPY
-        return addr > 0 && m_validRegisters.test(addr - 1) && m_values[addr - 1] == value;
+        // return addr > 0 && m_validRegisters.test(addr - 1) && m_values[addr - 1] == value;
+    if (addr > 0 && m_validRegisters.test(addr - 1) && m_values[addr - 1] == value) {
+        m_usage[addr - 1] = ++m_timestamp; // Recent use update
+        return true;
+    }
+        return false;
 #else
         return false;
 #endif
@@ -49,6 +54,7 @@ public:
         ASSURE_FALSE(m_validRegisters[reg - 1]);
         m_validRegisters.set(reg - 1);
         m_values[reg - 1] = value;
+        m_usage[reg - 1] = ++m_timestamp; // Update usage
     }
 
     uint8_t tmpRegister(mlir::Value) const
@@ -74,6 +80,25 @@ public:
         evictionIndex = (evictionIndex == maxEvictionIndex) ? 2 : evictionIndex + 1;
         ASSURE(evictionIndex, <=, maxEvictionIndex);
         return evictionIndex + 1;
+#elif EVICTION_STRATEGY == EVICTION_LRU
+        // in range [1, CACHE_REGISTERS] is valid (zero means not available)
+        const uint32_t v = static_cast<uint32_t>(~m_validRegisters.to_ulong())
+            & static_cast<uint32_t>(CLOBBER_ALL.to_ulong());
+        if (v) {
+            const auto lz = 32 - __builtin_clz(v);
+            return lz;
+        }
+        // No free registers available, select LRU for eviction
+        uint64_t minUsage = UINT64_MAX;
+        uint8_t lruIndex = 2; // start from first usable register
+        for (uint8_t i = 2; i < CACHE_REGISTERS; ++i) {
+            if (m_usage[i] < minUsage) {
+                minUsage = m_usage[i];
+                lruIndex = i;
+            }
+        }
+        ASSURE(lruIndex, <=, maxEvictionIndex);
+        return lruIndex + 1;
 #else
         return 0;
 #endif
@@ -94,6 +119,8 @@ public:
 private:
     ClobberMask m_validRegisters{};
     std::array<mlir::Value, CACHE_REGISTERS> m_values{};
+    std::array<uint64_t, CACHE_REGISTERS> m_usage{}; // Array to track the timestamps
+    uint64_t m_timestamp = 0; 
     mutable uint32_t evictionIndex = 2;
     uint32_t maxEvictionIndex = CACHE_REGISTERS - 1;
 };
